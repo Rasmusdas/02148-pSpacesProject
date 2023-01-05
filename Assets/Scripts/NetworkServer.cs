50e97d34ab87b7c87dfc6b77ce515de2e1a00e71
Rasmusdas <rasmusmkn@gmail.com>
2023-01-05 14:05:37 +0100

    Yeet4

--- Assets/Scripts/NetworkServer.cs
+++ Assets/Scripts/NetworkServer.cs
@@ -111,7 +111,9 @@ public class NetworkServer
         (int, float, float, float) data = ((int, float, float, float))packet.data;
         foreach (string id in _playerIds)
         {
-            if (id == networkObjectOwners[data.Item1]) { Debug.Log("Stopping packet to owner " + networkObjectOwners[data.Item1]);return; }
+            if (id == networkObjectOwners[data.Item1]) { Debug.Log("Stopping packet to owner " + networkObjectOwners[data.Item1]);continue; }
+
+            Debug.Log(id);
 
             _playerSpaces[id].Put(id, packet.type.ToString(), data.Item1, data.Item2, data.Item3, data.Item4);
         }
