ecade37cef6f45953f389d4739e8829221cfaeae
jaco8485 <50986008+jaco8485@users.noreply.github.com>
2023-01-06 09:23:00 +0100

    Rot stuf

--- Assets/Scripts/NetworkServer.cs
+++ Assets/Scripts/NetworkServer.cs
@@ -105,6 +105,12 @@ public class NetworkServer
 
         _serverSpace.Put(packet.target, packet.type.ToString(), data.Item1, data.Item2.x, data.Item2.y, data.Item2.z);
     }
+    internal static void RotationUpdate(Packet packet)
+    {
+        (int, Quaternion) data = ((int, Quaternion))packet.data;
+
+        _serverSpace.Put(packet.target, packet.type.ToString(), data.Item1, data.Item2.x, data.Item2.y, data.Item2.z, data.Item2.w);
+    }
 
     private static void BroadcastMovementUpdate(Packet packet)
     {
@@ -207,6 +213,7 @@ public class NetworkServer
         return new string(Enumerable.Repeat(chars, length)
             .Select(s => s[random.Next(s.Length)]).ToArray());
     }
+
 }
 
 public struct ServerInfo
@@ -250,5 +257,6 @@ public struct Packet
 
 public enum PacketType
 {
-    Movement,Instantiate
+    Movement,Instantiate,
+    Rotation
 }
